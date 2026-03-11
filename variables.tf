3a12056ee3c50697b8d431a97368a9b22f281118
PUND RUSHABH MOHAN <rushabpund@gmail.com>
2026-03-11 10:12:16 +0530

    Add Terraform variables for AWS deployment
    Define variables for AWS deployment configuration including region, project name, VPC CIDR, subnet CIDRs, instance type, key name, domain name, and database credentials.

---
+++ variables.tf
@@ -0,0 +1,64 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  description = "Project name for tagging"
+  type        = string
+  default     = "netflix-wp-streaming"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+}
+
+variable "domain_name" {
+  description = "Domain name for the website (optional)"
+  type        = string
+  default     = ""
+}
+
+variable "db_name" {
+  description = "WordPress database name"
+  type        = string
+  default     = "wordpress_db"
+}
+
+variable "db_user" {
+  description = "WordPress database user"
+  type        = string
+  default     = "wp_admin"
+}
+
+variable "db_password" {
+  description = "WordPress database password"
+  type        = string
+  sensitive   = true
+}
