ba1293b7502f085da81ba89c7c8817d7c9fe057a
PUND RUSHABH MOHAN <rushabpund@gmail.com>
2026-03-11 11:06:38 +0530

    Create CloudFront distribution for video CDN
    Added CloudFront distribution and origin access control for S3 bucket.

---
+++ cloudfront.tf
@@ -0,0 +1,57 @@
+# Origin Access Control for S3
+resource "aws_cloudfront_origin_access_control" "s3_oac" {
+  name                              = "${var.project_name}-s3-oac"
+  description                       = "OAC for video S3 bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "video_cdn" {
+  enabled             = true
+  comment             = "${var.project_name} Video CDN"
+  default_root_object = "index.html"
+
+  origin {
+    domain_name              = aws_s3_bucket.video_processed.bucket_regional_domain_name
+    origin_id                = "S3-video-processed"
+    origin_access_control_id = aws_cloudfront_origin_access_control.s3_oac.id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-video-processed"
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Name    = "${var.project_name}-video-cdn"
+    Project = var.project_name
+  }
+}
