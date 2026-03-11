2ddaefabd01ce1e30186f01c901d46123f4f7b49
PUND RUSHABH MOHAN <rushabpund@gmail.com>
2026-03-11 11:08:05 +0530

    Add Cognito User Pool and Client resources

---
+++ cognito.tf
@@ -0,0 +1,77 @@
+# Cognito User Pool
+resource "aws_cognito_user_pool" "main" {
+  name = "${var.project_name}-user-pool"
+
+  username_attributes      = ["email"]
+  auto_verified_attributes = ["email"]
+
+  password_policy {
+    minimum_length    = 8
+    require_lowercase = true
+    require_numbers   = true
+    require_symbols   = false
+    require_uppercase = true
+  }
+
+  schema {
+    name                = "email"
+    attribute_data_type = "String"
+    required            = true
+    mutable             = true
+
+    string_attribute_constraints {
+      min_length = 5
+      max_length = 100
+    }
+  }
+
+  schema {
+    name                = "name"
+    attribute_data_type = "String"
+    required            = true
+    mutable             = true
+
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 100
+    }
+  }
+
+  tags = {
+    Project = var.project_name
+  }
+}
+
+# Cognito User Pool Client
+resource "aws_cognito_user_pool_client" "wordpress_client" {
+  name         = "${var.project_name}-wp-client"
+  user_pool_id = aws_cognito_user_pool.main.id
+
+  generate_secret                      = false
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows                  = ["code", "implicit"]
+  allowed_oauth_scopes                 = ["email", "openid", "profile"]
+
+  callback_urls = [
+    "http://${aws_lb.main.dns_name}/wp-login.php",
+    "http://${aws_lb.main.dns_name}/"
+  ]
+
+  logout_urls = [
+    "http://${aws_lb.main.dns_name}/"
+  ]
+
+  supported_identity_providers = ["COGNITO"]
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_SRP_AUTH"
+  ]
+}
+
+# Cognito User Pool Domain
+resource "aws_cognito_user_pool_domain" "main" {
+  domain       = "${var.project_name}-auth"
+  user_pool_id = aws_cognito_user_pool.main.id
+}
